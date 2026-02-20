e708d7a5fe341446f89149d98982e248de218ea5
humaidhifraadh2-debug <humaidhifraadh2@gmail.com>
2026-02-20 21:15:48 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { CartProvider } from "./context/CartContext";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
-import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <CartProvider>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </CartProvider>
+  </React.StrictMode>
 );
